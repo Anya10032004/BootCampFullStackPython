d10fdf61b78ff23b61cae01caba2ef0f499b0fc2
Anya10032004 <122957165+Anya10032004@users.noreply.github.com>
2026-09-11 18:12:59 +0700

    11/09/26

--- lala.py
+++ lala.py
@@ -5,7 +5,6 @@ shopping_cart = {
     'Quantity of Item': []
 }
 num_of_items = 0
-choose = 0
 while m == 0:
     print("===================================")
     print("           Cassier Program         ")
@@ -17,56 +16,55 @@ while m == 0:
     print("3. checkout")
     print("4. Exit")
     print(" ")
-    choose = int(input("Choose menu: "))
+    choose = input("Choose menu: ")
+    choose = int(choose)
+    match choose:
+        case 1:
+             print("===================================")
+             print("           Add Item                ")
+             print("===================================")
+             item = input("Enter item name: ")
+             price = int(input("Enter item price: "))
+             quantity = int(input("Enter item quantity: "))
+             shopping_cart['Name of Item'].append(item)
+             shopping_cart['Price of Item'].append(price)
+             shopping_cart['Quantity of Item'].append(quantity)
+             num_of_items += 1
+             if len(shopping_cart['Name of Item']) == num_of_items and len(shopping_cart['Price of Item']) == num_of_items and len(shopping_cart['Quantity of Item']) == num_of_items:
+                    print("Item added successfully!")
+             else:
+                    print("Failed to add item.")
+        case 2:
+              print("===================================")
+              print("           List of Items           ")
+              print("===================================")
+              if num_of_items == 0:
+                    print("No items in the shopping cart.")
+              else:
+                    for i in range(num_of_items):
+                        print(f"Item {i + 1}:")
+                        print(f"Name: {shopping_cart['Name of Item'][i]}")
+                        print(f"Price: {shopping_cart['Price of Item'][i]}")
+                        print(f"Quantity: {shopping_cart['Quantity of Item'][i]}")
+                        print("-----------------------------------")
+              print("===================================")
+        case 3:
+              print("===================================")
+              print("           Checkout                ")
+              print("===================================")
+              if num_of_items == 0:
+                    print("No items in the shopping cart.")
+              else:
+                    total_price = 0
+                    for i in range(num_of_items):
+                         item_total = shopping_cart['Price of Item'][i] * shopping_cart['Quantity of Item'][i]
+                         total_price += item_total
+                    print(f"Total price: { total_price}")
+              print("===================================")
+        case 4:
+               print("Exiting the program.")
+               m = 1
 
-    if choose == 1:
-        print("===================================")
-        print("           Add Item                ")
-        print("===================================")
-        item = input("Enter item name: ")
-        price = int(input("Enter item price: "))
-        quantity = int(input("Enter item quantity: "))
-        shopping_cart['Name of Item'].append(item)
-        shopping_cart['Price of Item'].append(price)
-        shopping_cart['Quantity of Item'].append(quantity)
-        num_of_items += 1
-
-        if len(shopping_cart['Name of Item']) == num_of_items and len(shopping_cart['Price of Item']) == num_of_items and len(shopping_cart['Quantity of Item']) == num_of_items:
-            print("Item added successfully!")
-        else:
-            print("Failed to add item.")
-        print("===================================")
-
-    elif choose == 2:
-        print("===================================")
-        # print("           List of Items           ")
-        # print("===================================")
-        # if num_of_items == 0:
-        #     print("No items in the shopping cart.")
-        # else:
-        #     for i in range(num_of_items):
-        #         print(f"Item {i + 1}:")
-        #         print(f"Name: {shopping_cart['Name of Item'][i]}")
-        #         print(f"Price: {shopping_cart['Price of Item'][i]}")
-        #         print(f"Quantity: {shopping_cart['Quantity of Item'][i]}")
-        #         print("-----------------------------------")
-        # print("===================================")
-
-    elif choose == 3:
-        print("===================================")
-        # print("           Checkout                ")
-        # print("===================================")
-        # if num_of_items == 0:
-        #     print("No items in the shopping cart.")
-        # else:
-        #     total_price = 0
-        #     for i in range(num_of_items):
-        #         item_total = shopping_cart['Price of Item'][i] * shopping_cart['Quantity of Item'][i]
-        #         total_price += item_total
-        #         print(f"Total price: { total_price}")
-        #         print("-----------------------------------")
-        # print("===================================")
-
-    elif choose == 4:
-        print("Exiting the program.")
-        m = 1
+        case _:
+            print("Invalid choice. Please try again.")
+        
